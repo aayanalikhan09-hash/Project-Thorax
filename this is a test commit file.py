800a8db4270b100ae58d6e0f5bf25e8534b11d64
aayanalikhan09-hash <aayanalikhan09@gmail.com>
2026-06-15 16:30:01 -0400

    last commit for now i think

--- this is a test commit file.py
+++ this is a test commit file.py
@@ -2,3 +2,4 @@
 #try dos
 #alr this is to make sure its consistent
 #this is to test deleting stuff from git commits so lets try this now lol
+#i need to go to the slack and stuff to find where the pcb part of things is
